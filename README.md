1a363a5cec6943253c77d4d6c128e4bf7cacb999
anacondong <westkitseo@gmail.com>
2017-10-09 10:09:45 +0700

    spring-boot-AOP-AspectJ

---
+++ README.md
@@ -0,0 +1,21 @@
+# SpringAOP-AspectJ<br/>
+
+Using Spring Boot, AOP RestFul Controller <br/>
+JAVA 8<br/>
+
+#### Pre require
+Java 8<br/>
+Maven<br/>
+
+#### How to run: 
+mvn spring-boot:run <br/>
+
+#### Open browser url :
+
+http://localhost:8081/SpringAOP-AspectJ/aspectjoinpoint
+http://localhost:8081/SpringAOP-AspectJ/aspectpointcut
+http://localhost:8081/SpringAOP-AspectJ/aspectloggable
+http://localhost:8081/SpringAOP-AspectJ/aspectnormal
+
+<br/>
+Thanks and enjoy Coding
---
+++ java/com/sample/springaop/aspect/CustomerAspectPointcut.java
@@ -0,0 +1,28 @@
+package com.sample.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class CustomerAspectPointcut {
+
+	@Before("findCustomerByLastNamePointCut()")
+	public void loggingAdvice() {
+		System.out.println("Execute logAdvice on findCustomerByLastName()");
+	}
+
+	@Pointcut("execution(* com.sample.springaop.service.CustomerAnotherService.findCustomerByLastName(..))")
+	public void findCustomerByLastNamePointCut() {
+	}
+
+	@Before("allMethodsPointcut()")
+	public void allServiceMethodsAdvice(JoinPoint joinPoint) {
+		System.out.println("Execute logAdvice for AnotherService Method with joinPoint: " + joinPoint.getSignature().getName());
+	}
+
+	@Pointcut("within(com.sample.springaop.service.CustomerAnotherService)")
+	public void allMethodsPointcut() {
+	}
+}
